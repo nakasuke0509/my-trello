0a241a044e947404a1d88467506262749b093329
nakasuke <nakasuke0509.biz@gmail.com>
2020-04-29 18:29:50 +0900

    入力フォームの活性・非活性の切り替え処理を追加

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,9 +1,11 @@
 <template>
-　　<form class="addlist" @submit.prevent="addList">
-    　　<input v-model="title"
+    <form :class="classList" @submit.prevent="addList">
+        <input v-model="title"
                 type="text"
                 class="text-input"
                 placeholder="Add new list"
+                @focusin="startEditing"
+                @focusout="finishEditing"
         >
         <button type="submit" class="add-button">
             Add
@@ -16,12 +18,28 @@ export default {
   data: function() {
     return {
       title: '',
+      isEditing: false,
     }
   },
+  computed: {
+    classList() {
+            const classList = ['addlist']
+            if (this.isEditing) {
+                classList.push('active')
+            }
+            return classList
+        },
+    },
   methods: {
     addList: function() {
       this.$store.dispatch('addlist', { title: this.title })
       this.title = ''
+    },
+    startEditing() {
+        this.isEditing = true
+    },
+    finishEditing() {
+        this.isEditing = false
     },
   }
 }
